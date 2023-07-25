dd25c441b52ad9761e22742badb5e75625c3cfff
arypte <wjs505@gmail.com>
2023-07-25 16:00:14 +0900

    페이지 라우팅

---
+++ next_project_study/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-blue-300 min-h-screen flex justify-center items-center flex-col p-24">
+      <Image
+        src="/images/rainbow.jpg"
+        width={300}
+        height={200}
+        alt="무지개 배경"
+      />
+      <div>{process.env.NEXT_JS}</div>
+    </div>
+  );
+};
+
+export default Home ;
